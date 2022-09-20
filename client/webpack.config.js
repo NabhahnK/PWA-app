7323cf6e1ba3cc3c452b18d9ec210a48525a656a
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-09-20 14:10:08 -0700

    added service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,6 +23,12 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
     ],
 
     module: {
